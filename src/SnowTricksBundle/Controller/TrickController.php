65d17c30c45d7852430a3dc6022864f08e0c0159
Nabil001 <nabil.lemenuel@gmx.fr>
2017-11-29 22:52:15 +0100

    Implemented index and view actions, TrickRepository has 2 methods seeking to minimize the number of queries made to send a response

--- src/SnowTricksBundle/Controller/TrickController.php
+++ src/SnowTricksBundle/Controller/TrickController.php
@@ -4,43 +4,73 @@ namespace SnowTricksBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
+use SnowTricksBundle\Entity\Trick;
 
 class TrickController extends Controller
 {
     /**
-     * @Route("/{page}", name = "trick_index", requirements = {"page": "\d+"})
+     * @Route(
+     *      "/{page}",
+     *      name = "trick_index",
+     *      requirements = {"page": "\d+(?<!0)"}
+     * )
      */
     public function indexAction($page = 1)
     {
-        $tricks = $this
-            ->getDoctrine()
-            ->getManager()
-            ->getRepository('SnowTricksBundle:Trick')
-            ->getOrderedList();
+        $tricks =  $this->getDoctrine()
+                        ->getManager()
+                        ->getRepository('SnowTricksBundle:Trick')
+                        ->getOrderedList($page, $this->getParameter(
+                            'snowtricks.tricks.amount_per_page'
+                        ));
+
+        if(!$tricks->getIterator()->count())
+        {
+            throw $this->createNotFoundException();
+        }
 
         return $this->render('snowtricks/index.html.twig', array(
-            'tricks' => $tricks
+            'tricks' => $tricks,
+            'page' => $page,
         ));
     }
 
     /**
      * @Route("/trick/{slug}", name = "trick_view")
+     * @ParamConverter(
+     *      "trick",
+     *      options = {
+     *          "repository_method" = "findBySlugWithAll"
+     *      }
+     * )
      */
-    public function viewAction()
+    public function viewAction(Trick $trick)
     {
-        return $this->render('snowtricks/view.html.twig');
+        return $this->render('snowtricks/view.html.twig', array(
+            'trick' => $trick
+        ));
     }
 
-    public function deleteAction()
+    /**
+     * @Route("/delete/{slug}", name = "trick_delete")
+     */
+    public function deleteAction(Trick $trick)
     {
         return $this->render('snowtricks/delete.html.twig');
     }
 
+    /**
+     * @Route("/add", name = "trick_add")
+     */
     public function addAction()
     {
         return $this->render('snowtricks/add.html.twig');
     }
 
+    /**
+     * @Route("/edit/{slug}", name = "trick_edit")
+     */
     public function editAction()
     {
         return $this->render('snowtricks/edit.html.twig');
